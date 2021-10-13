d6aa500e5064768a4389cbf927b126e4b84a2f97
Jeswin-w <jeswinw07@gmail.com>
2021-10-13 16:07:34 +0530

    node 5

---
+++ workerex.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+var ObjectId = require('mongodb').ObjectId
+var url1 = "mongodb://localhost:27017/";
+ 
+
+var MongoClient = require('mongodb').MongoClient;
+const port = 3007;
+
+app.get()
+app.listen(port, () => console.log('The server running on Port '+port));
